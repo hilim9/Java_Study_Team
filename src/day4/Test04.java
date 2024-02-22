d6f37cc95f8723b2684354335eec4a299d0c6c97
hilim9 <dlagydls1212@naver.com>
2024-02-23 08:09:27 +0900

    day4 수정

--- src/day4/Test04.java
+++ src/day4/Test04.java
@@ -12,13 +12,15 @@ public class Test04 {
 
     public static int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
-        boolean result;
 
-
-        if (ineq.equals("<")) {
-            result = eq.equals("=")? n <= m : n < m;
+        if (ineq.equals("<") && eq.equals("=")) {
+            answer = n <= m? 1 : 0;
+        } else if (ineq.equals("<") && eq.equals("!")){
+            answer = n < m ? 1 : 0;
+        } else if (ineq.equals(">") && eq.equals("=")){
+            answer = n >= m? 1 : 0;
         } else {
-            result = eq.equals("=")? n >= m : n > m;
+            answer = n > m? 1 : 0;
         }
         return answer;
     }
