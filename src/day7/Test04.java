76a7bbaed2746220022fe2897b08169048b381dd
hilim9 <dlagydls1212@naver.com>
2024-02-27 22:15:27 +0900

    day7 4번 수정

--- src/day7/Test04.java
+++ src/day7/Test04.java
@@ -2,30 +2,34 @@ package day7;
 
 import java.util.Arrays;
 
-
 public class Test04 {
     public static void main(String[] args) {
 
         // 콜라츠 수열 만들기
+        // 모든 자연수 x에 대해서 현재 값이 x이면 x가 짝수일 때는 2로 나누고,
+        // x가 홀수일 때는 3 * x + 1로 바꾸는 계산을 계속해서 반복하면
+        // 언젠가는 반드시 x가 1이 되는지 묻는 문제를 콜라츠 문제라고 부릅니다.
+
 
         System.out.println("result: " + Arrays.toString(solution(10)));
 
     }
 
     public static int[] solution(int n) {
-        int[] answer = new int[7]; // 배열의 크기
-        answer[0] = n; // 배열 0번째 인덱스에 n값 넣어줌
-        int idx = 1; // 배열 인덱스를 증가시킬 변수
-        int num = n; // n을 담아 줄 변수
+        String str = n + " "; // 문자열로 변환
 
-        while (true) {
-            if (num != 1) {
-                num = num % 2 == 0 ? num / 2 : 3 * num + 1;
-                answer[idx++] = num;
-            } else {
-                break;
-            }
+        while (true) { // 콜라츠 수열 만들기
+            if (n != 1) { // 1이 아닐 때
+                n = n % 2 == 0 ? n / 2 : 3 * n + 1;
+                str += n + " ";
+            } else break; // 1일 때
+        }
+        String[] strings = str.split(" "); // 공백을 기준으로 잘라 배열에 담음
+        int[] answer = new int[strings.length]; // int 배열
+        for (int i = 0; i < strings.length; i++) { // int 배열에 담아 줌
+            answer[i] = Integer.parseInt(strings[i]);
         }
         return answer;
+
     }
 }
