a8f5d9fc7b6b9c8f313e430305c46853c66e8e34
hilim9 <dlagydls1212@naver.com>
2024-02-28 00:17:19 +0900

    day7 2번 5번

---
+++ src/day7/Test05.java
@@ -0,0 +1,31 @@
+package day7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test05 {
+    public static void main(String[] args) {
+
+        // 배열 만들기 4
+
+        int[] arr = {1, 4, 2, 5, 3};
+
+        System.out.println("result: " + solution(arr));
+
+    }
+
+    public static List<Integer> solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+
+        for(int i = 0; i < arr.length;) { // arr의 길이만큼 반복
+            if (stk.size() == 0) {  // stk가 빈 배열 일 때
+                stk.add(arr[i]);    // arr[i]를 stk에 추가
+                i++;                // i 증가
+            } else if ((stk.get(stk.size() -1) < arr[i])) { // stk의 마지막 원소가 arr[i]보다 작을 때
+                stk.add(arr[i]);    // arr[i]를 추가
+                i++;                // i 증가
+            } else stk.remove(stk.size() -1); // 둘다 아닐 때 stk의 마지막 원소를 제거
+        }
+        return stk;
+    }
+}
