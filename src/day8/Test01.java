be438213a06ae4c94757a4a18e3ab8bc6800e20d
hilim9 <dlagydls1212@naver.com>
2024-02-28 18:50:35 +0900

    day8 1번 2번 5번

---
+++ src/day8/Test01.java
@@ -0,0 +1,20 @@
+package day8;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        // 간단한 논리 연산
+
+        System.out.println("result1: " + solution(false, true, true, true));
+        System.out.println("result2: " + solution(true, false, false, false));
+
+    }
+
+    public static boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = true;
+
+        answer = (x1 || x2) && (x3 || x4); // true or false 값을 answer에 담는다.
+
+        return answer;
+    }
+}
