7a438e8c838210de6dfa1e220183f0f659c021f8
hilim9 <dlagydls1212@naver.com>
2024-02-29 18:09:09 +0900

    day10

---
+++ src/day10/Test04.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class Test04 {
+    public static void main(String[] args) {
+
+        // 세로 읽기
+
+    }
+
+    public static String solution(String my_string, int m, int c) {
+        String answer = "";
+        return answer;
+    }
+}
---
+++ src/day10/Test05.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class Test05 {
+    public static void main(String[] args) {
+
+        // qr code
+
+    }
+
+    public static String solution(int q, int r, String code) {
+        String answer = "";
+        return answer;
+    }
+}
