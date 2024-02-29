02dc8ce9944e58faeae52c54440e675e64cb2944
hilim9 <dlagydls1212@naver.com>
2024-02-29 19:06:51 +0900

    day10 4번 수정

--- src/day10/Test04.java
+++ src/day10/Test04.java
@@ -6,8 +6,8 @@ public class Test04 {
     public static void main(String[] args) {
 
         // 세로 읽기
-        System.out.println(solution("ihrhbakrfpndopljhygc", 4, 2));
-        System.out.println(solution("programmers", 1, 1));
+        System.out.println("result: " + solution("ihrhbakrfpndopljhygc", 4, 2));
+        System.out.println("result: " + solution("programmers", 1, 1));
     }
 
     public static String solution(String my_string, int m, int c) {
