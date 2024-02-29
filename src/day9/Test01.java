cfce3646eb4759491aee115eafd5e77aca799fbe
hilim9 <dlagydls1212@naver.com>
2024-02-29 17:24:32 +0900

    day9

---
+++ src/day9/Test01.java
@@ -0,0 +1,28 @@
+package day9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        // 배열 만들기 5
+
+        String[] intStrs = {"0123456789","9876543210","9999999999999"};
+
+        System.out.println("result: " + solution(intStrs, 50000, 5, 5));
+
+    }
+
+    public static List<Integer> solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+
+        for(String num : intStrs) {
+            String str = num.substring(s, s+l); // s번 인덱스에서 s+l번 인덱스 전까지 해당하는 문자열을 잘라서 str에 담아준다
+            if (k < Integer.parseInt(str)) list.add(Integer.parseInt(str));
+            // 잘라낸 문자열이 k보다 큰경우에만 list에 담아준다.
+        }
+
+        return list;
+    }
+}
---
+++ src/day9/Test04.java
@@ -0,0 +1,24 @@
+package day9;
+
+import java.util.Arrays;
+
+public class Test04 {
+    public static void main(String[] args) {
+
+        // 접미사 배열
+        System.out.println(Arrays.toString(solution("banana")));
+        System.out.println(Arrays.toString(solution("programmers")));
+    }
+
+    public static String[] solution(String my_string) {
+        String[] answer = new String[my_string.length()];
+
+        for (int i = 0, j = 1; i < my_string.length(); i++, j++) {
+            answer[i] = my_string.substring(my_string.length() -j); // 접미사를 answer배열에 담아준다.
+        }
+
+        Arrays.sort(answer); // 사전순으로 정렬
+
+        return answer;
+    }
+}
