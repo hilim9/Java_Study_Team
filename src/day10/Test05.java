c4678f922b0b3eafdadf32aa8a9895489f1c2b3b
hilim9 <dlagydls1212@naver.com>
2024-02-29 19:05:38 +0900

    day10 4번 5번 수정

--- src/day10/Test05.java
+++ src/day10/Test05.java
@@ -4,11 +4,18 @@ public class Test05 {
     public static void main(String[] args) {
 
         // qr code
-
+        System.out.println("result: " + solution(3, 1, "qjnwezgrpirldywt"));
+        System.out.println("result: " + solution(1, 0, "programmers"));
     }
 
     public static String solution(int q, int r, String code) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < code.length(); i++) {
+            if (i % q == r) { // code의 인덱스를 i로 나누었을 때 나머지가 r인 경우
+                answer.append(code.charAt(i));
+            }
+        }
+        return answer.toString();
     }
 }
