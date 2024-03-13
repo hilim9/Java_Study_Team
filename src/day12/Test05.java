28c12d2ae3b80b937f62d5367b38b2c18a02758a
hilim9 <dlagydls1212@naver.com>
2024-03-13 17:59:26 +0900

    day12 5번 수정

--- src/day12/Test05.java
+++ src/day12/Test05.java
@@ -1,6 +1,7 @@
 package day12;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test05 {
@@ -11,36 +12,19 @@ public class Test05 {
         int[] arr = {0, 1, 2, 3, 4, 5};
         int[] query = {4, 1, 2};
 
-        System.out.println("result: " + solution(arr, query));
+        System.out.println("result: " + Arrays.toString(solution(arr, query)) );
     }
 
-    public static List<Integer> solution(int[] arr, int[] query) {
-        List<Integer> list = new ArrayList<>();
+    public static int[] solution(int[] arr, int[] query) {
 
-        for (int n : arr) {
-            list.add(n);
-        }
-
-        // System.out.println(list);
-
-        for (int i = 0; i < query.length; i++) {
+        for(int i =0; i<query.length; i++) {
             if(i % 2 == 0) { // 짝수 일 때
-                for (int even = query[i]+1; even < list.size(); even++) {
-                    System.out.println(query[i]);
-                    System.out.println("짝"+even);
-                    list.remove(even);
-                    System.out.println(list);
-                }
-            } else { // 홀수 일 때
-                for (int odd = 0; odd < query[i]; odd++) {
-                    System.out.println(query[i]);
-                    System.out.println("홀"+ odd);
-                    list.remove(odd);
-                    System.out.println(list);
-                }
+                arr = Arrays.copyOfRange(arr, 0, query[i]+1);
+            }else { // 홀수 일 때
+                arr = Arrays.copyOfRange(arr, query[i], arr.length);
             }
         }
-        return list;
+        return arr;
     }
 
     /*public static List<Integer> solution(int[] arr, int[] query) {
