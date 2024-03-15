732ef4b3902885487000032d65398064515190fc
hilim9 <dlagydls1212@naver.com>
2024-03-15 20:45:42 +0900

    day13 4번 5번

---
+++ src/day13/Test04.java
@@ -0,0 +1,28 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test04 {
+    public static void main(String[] args) {
+
+        // n번째 원소까지
+
+        int[] num_list1 = {2, 1, 6};
+        int[] num_list2 = {5, 2, 1, 7, 5};
+
+        System.out.println("result: " + solution(num_list1, 1));
+        System.out.println("result: " + solution(num_list2, 3));
+
+    }
+
+    public static List<Integer> solution(int[] num_list, int n) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            list.add(num_list[i]);
+        }
+
+        return list;
+    }
+}
---
+++ src/day13/Test05.java
@@ -0,0 +1,27 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test05 {
+    public static void main(String[] args) {
+
+        // n개 간격의 원소들
+
+        int[] num_list = {4, 2, 6, 1, 7, 6};
+
+        System.out.println("result: " + solution(num_list, 2));
+        System.out.println("result: " + solution(num_list, 4));
+
+    }
+
+    public static List<Integer> solution(int[] num_list, int n) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < num_list.length; i+=n) {
+            list.add(num_list[i]);
+        }
+
+        return list;
+    }
+}
