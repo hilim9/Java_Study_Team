cfb44fc4758ac645e200d099320031d38c9572a0
hilim9 <dlagydls1212@naver.com>
2024-04-03 17:57:31 +0900

    day17 all

---
+++ src/day17/Test03.java
@@ -0,0 +1,29 @@
+package day17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test03 {
+    public static void main(String[] args) {
+
+        // ad 제거하기
+
+        String[] strArr1 = {"and","notad","abcd"};
+        String[] strArr2 = {"there","are","no","a","ds"};
+
+        System.out.println("result: " + solution(strArr1));
+        System.out.println("result: " + solution(strArr2));
+
+    }
+
+    public static List<String> solution(String[] strArr) {
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < strArr.length; i++) {
+            if (!strArr[i].contains("ad")) list.add(strArr[i]); // ad가 포함되지 않았을 때 list에 추가
+        }
+
+        return list;
+    }
+
+}
---
+++ src/day17/Test05.java
@@ -0,0 +1,21 @@
+package day17;
+
+import java.util.Arrays;
+
+public class Test05 {
+    public static void main(String[] args) {
+
+        // 공백으로 구분하기 2
+
+        System.out.println("result: " + Arrays.toString(solution(" i    love  you")));
+        System.out.println("result: " + Arrays.toString(solution("    programmers  ")));
+
+    }
+
+    public static String[] solution(String my_string) {
+        String[] answer = my_string.trim().split("\\s+"); // \s: 공백 +: 1번 이상
+
+        return answer;
+    }
+
+}
