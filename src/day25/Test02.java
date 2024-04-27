5fed5a6ec6ff2737f08a1b9f75513869b1cab5ba
hilim9 <dlagydls1212@naver.com>
2024-04-27 20:17:48 +0900

    day25 2번

---
+++ src/day25/Test02.java
@@ -0,0 +1,30 @@
+package day25;
+
+public class Test02 {
+    public static void main(String[] args) {
+
+        // 특별한 이차원 배열 2
+
+        int[][] arr1 = {{5, 192, 33}, {192, 72, 95}, {33, 95, 999}};
+        int[][] arr2 = {{19, 498, 258, 587}, {63, 93, 7, 754}, {258, 7, 1000, 723}, {587, 754, 723, 8}};
+
+        System.out.println("result: " + solution(arr1));
+        System.out.println("result: " + solution(arr2));
+
+    }
+
+    public static int solution(int[][] arr) {
+        int answer = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] != arr[j][i]) {
+                    answer = 0;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
